03f758d9c3cb09572dead05b15d83573b3d93b73
Ann Golubeva <mrako142@mail.ru>
2018-06-24 13:38:59 +0300

    Add files via upload

---
+++ src/Gallery.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Gallery from 'react-photo-gallery';
+
+
+class Sample extends React.Component {
+    render() {
+
+	return (
+        <div> <h2>Галерея</h2>
+	    <Gallery photos={PHOTO_SET} />
+	    </div>
+	);
+    }
+}
+const PHOTO_SET = [
+  {
+    src: 'https://s8.hostingkartinok.com/uploads/images/2018/06/d66ad6142baf09303b72a02b9c0c1682.png',
+    width: 1,
+    height: 1
+  },
+  
+  {
+    src: 'https://s8.hostingkartinok.com/uploads/images/2018/06/d66ad6142baf09303b72a02b9c0c1682.png',
+    width: 1,
+    height: 1
+  },
+
+  {
+    src: 'https://s8.hostingkartinok.com/uploads/images/2018/06/d66ad6142baf09303b72a02b9c0c1682.png',
+    width: 1,
+    height: 1
+  }
+];
+
+export default Sample;
---
+++ src/Map.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Map = () =>
+	(<div><h2>Карта</h2> <iframe src="https://yandex.ru/maps/?um=constructor%3A7bc5a5becc55c0360154e16ddca3ae764d662f2ebabbc141dda4e8a12cbd32a9&source=constructorLink" width="500" height="400" frameborder="0"></iframe> </div>);
+
+export default Map;
