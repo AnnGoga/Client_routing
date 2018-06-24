eed2248d7376096d1e4fc8d850f6380ef01e170a
Ann Golubeva <mrako142@mail.ru>
2018-06-24 23:01:56 +0300

    Update Counter.jsx

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,35 +6,31 @@ class Counter extends C {
     const { stars } = props;
     this.state = { stars };
   }
-	
   componentWillReceiveProps(nextProps) {
     this.setState({ stars: nextProps.stars });
   }
-  plus() {
+
+  buttonClickPlus(){
     const stars = +this.state.stars + 1;
     this.setState({ stars });
   }
-  
-  minus() {
+  buttonClickMinus(){
     if (+this.state.stars <= 0)
-			return;
-		const stars = +this.state.stars - 1;
-		this.setState({ stars });
+      return;
+    const stars = +this.state.stars - 1;
+    this.setState({ stars });
   }
-  
   render() {
     return (
-      <div><h3>Лайкер</h3> <hr />
-        <button class="btn btn-light" onClick={() => this.minus()}><span role="img">➖</span></button>
-        <button class="btn btn-light" onClick={() => this.plus()}><span role="img" aria-label="add">➕</span></button> 
+      <div><hr />
+        <button onClick={() => this.buttonClickPlus()}><span role="img" aria-label="add">➕</span></button>
+        <button onClick={() => this.buttonClickMinus()}><span role="img">➖</span></button>
         <Stars length={this.state.stars} />
-    </div>);
+      </div>);
   }
 }
 const Stars = ({ length }) =>
   <span>{Array.from({ length }, (v, i) => ++i).map(() => '⭐️').join('')}</span>;
 
 
-export default () => (
-  <div><Counter stars= "1" />
-</div>); 
+export default Counter;
