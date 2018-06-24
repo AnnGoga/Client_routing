069f154a62edf3368176eb3aff7ddd71e13404d7
Ann Golubeva <mrako142@mail.ru>
2018-06-24 23:01:10 +0300

    Update Gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,35 +1,12 @@
 import React from 'react';
-import Gallery from 'react-photo-gallery';
 
+const Gallery = () =>
+	(<div>
+		<h1>Галерея</h1>
+		<p>
+			<img src="https://cdn-images-1.medium.com/max/1468/1*kt9otqHk14BZIMNruiG0BA.png" width="734" height="238"  alt=""/>
+			<img src="https://docs.webix.com/media/desktop/date_picker.png" alt=""/>
+		</p>
+		</div>)
 
-class Sample extends React.Component {
-    render() {
-
-	return (
-        <div> <h2>Галерея</h2>
-	    <Gallery photos={PHOTO_SET} />
-	    </div>
-	);
-    }
-}
-const PHOTO_SET = [
-  {
-    src: 'https://s8.hostingkartinok.com/uploads/images/2018/06/d66ad6142baf09303b72a02b9c0c1682.png',
-    width: 1,
-    height: 1
-  },
-  
-  {
-    src: 'https://s8.hostingkartinok.com/uploads/images/2018/06/d66ad6142baf09303b72a02b9c0c1682.png',
-    width: 1,
-    height: 1
-  },
-
-  {
-    src: 'https://s8.hostingkartinok.com/uploads/images/2018/06/d66ad6142baf09303b72a02b9c0c1682.png',
-    width: 1,
-    height: 1
-  }
-];
-
-export default Sample;
+export default Gallery;
